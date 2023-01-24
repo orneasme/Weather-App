e937753da861fa62c10ad158cfa7655b7c9f1005
orneasme <123243041+orneasme@users.noreply.github.com>
2023-01-24 15:00:01 +0100

    Added wind speed, humidity and sky description
    Added wind speed, humidity and sky description to Display Weather function

--- src/app.js
+++ src/app.js
@@ -7,13 +7,22 @@ function getTemperature(response) {
 }
 
 function displayTemperature(response) {
+  console.log(response.data);
   let Temperature = Math.round(response.data.main.temp);
   let degrees = document.querySelector("#temperature");
   degrees.innerHTML = Temperature;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let Skydescription = document.querySelector("#description");
+  Skydescription.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "53534e195e419f621c24a11bb65c4126";
-let city = "Paris";
+let city = "Bariloche";
 let apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
 axios.get(apiURL).then(getTemperature);
