741680c593c54525aea5ccc458a3c5317aa8a1e0
orneasme <123243041+orneasme@users.noreply.github.com>
2023-01-24 15:33:24 +0100

    Added current time display

--- src/app.js
+++ src/app.js
@@ -18,7 +18,33 @@ function displayTemperature(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
   let wind = document.querySelector("#wind");
-  wind.innerHTML = response.data.wind.speed;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    `Tuesday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+  ];
+  let day = days[date.getDay()];
+
+  return ` ${day}, ${hours}:${minutes}`;
 }
 
 let apiKey = "53534e195e419f621c24a11bb65c4126";
